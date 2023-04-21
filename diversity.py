e447a0e997f4bc57fb536bf4c94d66396581de45
Matthew Stubenberg <matthewstubenberg@gmail.com>
2023-04-20 17:15:08 -1000

    Added diversity tab

---
+++ diversity.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas
+import plotly.express as px
+
+def createRawAndPercentGraph(df,selected_schools,title,yaxis):
+    #Bar Passage Full Time Long Term
+    selected_schools_df = df.loc[df['schoolname'].isin(selected_schools)]
+    fig_raw = px.line(selected_schools_df,x='CalendarYear',y=yaxis[0],color='schoolname',title="Raw <br>" + title)
+    #Percentage Numbers
+    fig_percentage = px.line(selected_schools_df,x='CalendarYear',y=yaxis[1],color='schoolname',title="Percentage <br>" + title)
+    return [fig_raw,fig_percentage]
+
+def createRawAndPercentGraphSingleSchool(df,selected_schools,title,yaxis):
+    #Bar Passage Full Time Long Term
+    selected_schools_df = df.loc[df['schoolname'].isin(selected_schools)]
+    fig_raw = px.line(selected_schools_df,x='CalendarYear',y=yaxis,title="Raw <br>" + title)
+    #Percentage Numbers
+    yaxis_percent = [ax + "_Percentage" for ax in yaxis]
+    fig_percentage = px.line(selected_schools_df,x='CalendarYear',y=yaxis_percent,title="Percentage <br>" + title)
+    return [fig_raw,fig_percentage]
+
+def displayDiversity(streamlit_tab,selected_schools):
+    streamlit_tab.header("Student Diversity")
+    df = pandas.read_csv('Enrollment.csv')
+    races = ['WhiteJD1','BlackJD1','AsianJD1','HispanicJD1','NatHawJD1','TwoorMoreJD1']
+    genders = ['MenJD1','WomenJD1']
+    race_genders = []
+    for race in races:
+        for gender in genders:
+            race_genders.append(race.replace("JD1","") +  gender)
+    #Calculate Percentages for incoming class by race
+    for race in races:
+        df[race+"_Percentage"] = df[race]/df['TotalJD1'] * 100
+
+    #Calculate Percentages for incoming class by gender
+    for gender in genders:
+        df[gender+"_Percentage"] = df[gender]/df['TotalJD1'] * 100
+
+    #Calculate Percentages for incoming class by gender and race
+    for race_gender in race_genders:
+        df[race_gender+"_Percentage"] = df[race_gender]/df['TotalJD1'] * 100
+
+    #Incoming Class by Race - Single School
+    streamlit_tab.subheader("Student Diversity Incoming Class")
+    race_choices = streamlit_tab.multiselect("Select Race",races,default=races)
+    if(len(selected_schools) > 1):
+        streamlit_tab.write("You can only have one school selected to use this chart")
+    else:
+        col1,col2 = streamlit_tab.columns(2)
+        title = "Student Diversity Race Incoming Class"
+        yaxis = race_choices
+
+        fig_graphs = createRawAndPercentGraphSingleSchool(df,selected_schools,title,yaxis)
+        col1.plotly_chart(fig_graphs[0])
+        col2.plotly_chart(fig_graphs[1])
+
+    #Incoming Class by Gender - Single School
+    streamlit_tab.subheader("Student Diversity Incoming Class")
+    if(len(selected_schools) > 1):
+        streamlit_tab.write("You can only have one school selected to use this chart")
+    else:
+        col1,col2 = streamlit_tab.columns(2)
+        title = "Student Diversity Gender Incoming Class"
+        yaxis = genders
+
+        fig_graphs = createRawAndPercentGraphSingleSchool(df,selected_schools,title,yaxis)
+        col1.plotly_chart(fig_graphs[0])
+        col2.plotly_chart(fig_graphs[1])
+
+    #Incoming Class by Race and Gender - Single School
+    streamlit_tab.subheader("Student Diversity Incoming Class")
+    race_gender_choices = streamlit_tab.multiselect("Select Race and Gender",race_genders,default=['AsianMenJD1','AsianWomenJD1'])
+    if(len(selected_schools) > 1):
+        streamlit_tab.write("You can only have one school selected to use this chart")
+    else:
+        col1,col2 = streamlit_tab.columns(2)
+        title = "Student Diversity Race and Gender Incoming Class"
+        yaxis = race_gender_choices
+
+        fig_graphs = createRawAndPercentGraphSingleSchool(df,selected_schools,title,yaxis)
+        col1.plotly_chart(fig_graphs[0])
+        col2.plotly_chart(fig_graphs[1])
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,12 @@ import pandas
 import plotly.express as px
 from employment import displayEmployment
 from admissions import displayAdmissions
+from diversity import displayDiversity
 from ubp import displayBarPassage
 
 st.set_page_config(layout='wide')
 st.header("Law School Analyzer")
-admissions_tab,employment_tab,bar_passage_tab = st.tabs(['Admissions','Employment','Bar Passage'])
+admissions_tab,employment_tab,bar_passage_tab,diversity_tab = st.tabs(['Admissions','Employment','Bar Passage','Diversity'])
 school_df = pandas.read_csv('School Information.csv')
 #schools = ['University of Hawaii','University of New Mexico']
 schools = school_df['schoolname'].sort_values()
@@ -18,3 +19,4 @@ selected_schools = st.sidebar.multiselect("Select Schools",schools,default='Univ
 displayEmployment(employment_tab,selected_schools)
 displayAdmissions(admissions_tab,selected_schools)
 displayBarPassage(bar_passage_tab,selected_schools)
+displayDiversity(diversity_tab,selected_schools)
